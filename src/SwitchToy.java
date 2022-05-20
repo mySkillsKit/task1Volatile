f5c5ed13faa8077f7dded56197fefcd2c6d48944
Anatolii Vasilev <avas2012@gmail.com>
2022-05-20 15:53:36 +0400

    second commit

--- src/SwitchToy.java
+++ src/SwitchToy.java
@@ -1,49 +1,39 @@
 public class SwitchToy {
 
     private final int REPEAT = 4; //number of iterations on - off toggle switch
-    int count;
-    volatile int volatileSwitch = 0; // switch off = 0 / switch on = 1;
-    int timeOn = 3000; //switch on time
-    int timeOff = 2000; //switch off time
+    private int count;
+    private volatile boolean volatileSwitch = false; // switch off = false / switch on = true;
+    private final int timeOn = 5000; //switch on time
 
     public void switchOn() {
 
         while (count != REPEAT) {
-
             try {
-                if (volatileSwitch == 0) {
-                    volatileSwitch = 1;
+                if (!volatileSwitch) {
                     count++;
+                    volatileSwitch = true;
                     System.out.println("Turning on the toggle switch / " + Thread.currentThread().getName());
-                    Thread.sleep(timeOn);
                 } else {
                     break;
                 }
-
+                Thread.sleep(timeOn);
             } catch (InterruptedException exc) {
                 exc.printStackTrace();
             }
         }
-
     }
 
     public void switchOff() {
 
         while (!Thread.currentThread().isInterrupted()) {
 
-            try {
-                Thread.sleep(timeOff);
-                if (volatileSwitch == 1) {
-                    volatileSwitch = 0;
-                    System.out.println("Turning off the toggle switch / " + Thread.currentThread().getName());
-                } else {
-                    break;
-                }
-
-            } catch (InterruptedException exc) {
-                exc.printStackTrace();
+            if (volatileSwitch) {
+                volatileSwitch = false;
+                System.out.println("Turning off the toggle switch / " + Thread.currentThread().getName());
             }
+
         }
+
     }
 
 }
